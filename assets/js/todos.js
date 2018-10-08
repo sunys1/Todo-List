7694a5117f5d5d357b938a1a55a2227dc62a584c
sunys1 <yizhou.sun78@gmail.com>
2018-10-08 17:47:43 -0400

    Use .on rather than .click to account for future potential elements which are not there when the code is run the first time

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,10 @@
 //Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
