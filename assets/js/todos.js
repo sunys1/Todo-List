492a821518531f1bae3caa4ad75d8197b94bf621
sunys1 <yizhou.sun78@gmail.com>
2018-10-10 20:47:11 -0400

    Add click event to the plus sign to add a fade in and out effecwhen clicking on it

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,11 @@ $("input[type='text']").keypress(function(){
 		var todo = $(this).val();
 		$(this).val("");
 		//create a new li with the new value in ul
+	opacity: 0;
 		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todo + "</li>");
 	}
+})
+
+$(".fa-plus-square").click(function(){
+	$("input[type='text'").fadeToggle();
 })
